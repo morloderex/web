53c2560a963b61075515415176893a989e3d59ef
sasin91 <jonas.kerwin.hansen@gmail.com>
2016-05-02 12:45:41 +0200

    added User active on registration config option

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -125,7 +125,7 @@ class AuthController extends Controller
         return User::create([
             // Users created are by default not active
             // eg. require email confirmation or Administrative intervention.
-            'active'    => False,
+            'active'    => Config('auth.user--active'),
             'name'      => $data['name'],
             'email'     => $data['email'],
             'password'  => $data['password'],
